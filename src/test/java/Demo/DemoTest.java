b50bb2f4e7abaf80398f34eb071b059f82919638
Arish <Arish@DESKTOP-L4G24IU>
2025-12-04 10:39:52 +0530

    Commited

---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,12 @@
+package Demo;
+import org.testng.annotations.Test;
+public class DemoTest {
+		@Test
+		 public void virat() 
+		{
+			  System.out.println("Virat");
+		  }
+
+	}
+
+
---
+++ src/test/java/Demo/SampleTest.java
@@ -0,0 +1,11 @@
+package Demo;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test(groups="regression")
+	 public void dhoni() 
+	{
+		  System.out.println("Dhoni");
+	  }
+
+}
